3ce25020a6dd6179ee669db94f193e1ee7983b9c
kamilevaic <106099595+kamilevaic@users.noreply.github.com>
2022-10-09 17:53:06 +0300

    task complete

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  this.sum = (a, b) => a + b;
+  this.subtraction = (a, b) => a - b;
+  this.multiplication = (a, b) => a * b;
+  this.division = (a, b) => a / b;
+}
+
+const result = new Calculator();
+console.log(result.sum(2, 2));
+console.log(result.subtraction(2, 2));
+console.log(result.multiplication(2, 2));
+console.log(result.division(2, 2));
